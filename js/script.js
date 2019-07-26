1c950353b062598a1cde0b5e969d8aad7958bff8
Joselyne97 <musosoaimee@gmail.com>
2019-07-26 19:41:12 +0200

    I did the hover

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
  $(".pic3").toggle();
     });
  });
- 
+
  $(document).ready(function(){
     $("form").submit(function(){
         var name = document.getElementById("name");
@@ -44,3 +44,125 @@ $(document).ready(function() {
       alert("Hey " + name.value + "! We have received your message,Thank You!");
     });
   });
+
+  $(".im1").hide();
+  $("#a").animate({
+      opacity:1
+  });
+  $('#a').hover(function(){
+      $(this).stop().animate({
+          opacity: .4},200);
+     $('.im1').fadeIn();
+  },
+  function(){
+      $(this).stop().animate({
+          opacity: 1},500);
+          $('.im1').fadeOut();
+      });
+//  .............................
+      $(".im2").hide();
+  $("#b").animate({
+      opacity:1
+  });
+  $('#b').hover(function(){
+      $(this).stop().animate({
+          opacity: .4},200);
+     $('.im2').fadeIn();
+  },
+  function(){
+      $(this).stop().animate({
+          opacity: 1},500);
+          $('.im2').fadeOut();
+      });
+    //   .............................
+    $(".im3").hide();
+  $("#c").animate({
+      opacity:1
+  });
+  $('#c').hover(function(){
+      $(this).stop().animate({
+          opacity: .4},200);
+     $('.im3').fadeIn();
+  },
+  function(){
+      $(this).stop().animate({
+          opacity: 1},500);
+          $('.im3').fadeOut();
+      });
+
+    // .............................
+    $(".im4").hide();
+  $("#d").animate({
+      opacity:1
+  });
+  $('#d').hover(function(){
+      $(this).stop().animate({
+          opacity: .4},200);
+     $('.im4').fadeIn();
+  },
+  function(){
+      $(this).stop().animate({
+          opacity: 1},500);
+          $('.im4').fadeOut();
+      });
+    // .............................
+    $(".im5").hide();
+    $("#e").animate({
+        opacity:1
+    });
+    $('#e').hover(function(){
+        $(this).stop().animate({
+            opacity: .4},200);
+       $('.im5').fadeIn();
+    },
+    function(){
+        $(this).stop().animate({
+            opacity: 1},500);
+            $('.im5').fadeOut();
+        });
+    //.............................
+    $(".im6").hide();
+    $("#f").animate({
+        opacity:1
+    });
+    $('#f').hover(function(){
+        $(this).stop().animate({
+            opacity: .4},200);
+       $('.im6').fadeIn();
+    },
+    function(){
+        $(this).stop().animate({
+            opacity: 1},500);
+            $('.im6').fadeOut();
+        });
+    //.............................
+    $(".im7").hide();
+  $("#g").animate({
+      opacity:1
+  });
+  $('#g').hover(function(){
+      $(this).stop().animate({
+          opacity: .4},200);
+     $('.im7').fadeIn();
+  },
+  function(){
+      $(this).stop().animate({
+          opacity: 1},500);
+          $('.im7').fadeOut();
+      });
+    //.............................
+    $(".im8").hide();
+    $("#h").animate({
+        opacity:1
+    });
+    $('#h').hover(function(){
+        $(this).stop().animate({
+            opacity: .4},200);
+       $('.im8').fadeIn();
+    },
+    function(){
+        $(this).stop().animate({
+            opacity: 1},500);
+            $('.im8').fadeOut();
+        });
+
