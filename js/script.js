0bef46312bb2c9aabc1c24b42f3ebdbf243a34b9
Joselyne97 <musosoaimee@gmail.com>
2019-07-26 10:20:31 +0200

    I hidden and shown the paragraphs in what we do

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
-$(document).ready (function(){
-   $(".pic1, .dede").click(function() {
-$("#pic3").toggle();
+$(document).ready(function() {
+   $("#pic1").click(function() {
 $(".dede").toggle();
+$(".pic1").toggle();
    });
 });
+$(document).ready(function() {
+    $("#par1").click(function() {
+ $(".dede").toggle();
+ $(".pic1").toggle();
+    });
+ });
+
+$(document).ready(function() {
+    $("#pic2").click(function() {
+ $(".deve").toggle();
+ $(".pic2").toggle();
+    });
+ });
+ $(document).ready(function() {
+    $("#deve").click(function() {
+ $(".deve").toggle()
+ ;$(".pic2").toggle();
+    });
+ });
+ $(document).ready(function() {
+    $("#pic3").click(function() {
+ $(".pro").toggle();
+ $(".pic3").toggle();
+    });
+ });
+ $(document).ready(function() {
+    $("#pro").click(function() {
+ $(".pro").toggle();
+ $(".pic3").toggle();
+    });
+ });
